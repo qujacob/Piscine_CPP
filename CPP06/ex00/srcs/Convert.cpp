ddf41afb96a3e727328bb28198f48f1109c79854
JACOB QUENTIN <quentin.jacob2@etu.unistra.fr>
2022-07-10 22:51:52 +0200

    CPP06 ex00 fini

--- CPP06/ex00/srcs/Convert.cpp
+++ CPP06/ex00/srcs/Convert.cpp
@@ -35,40 +35,59 @@ e_types	Convert::whatType(void) {
 void	Convert::print(e_types type) {
 	switch (type) {
 		case CHAR :
-			std::cout << "char: " << this->_argChar << std::endl;
-			std::cout << "int: " << static_cast<int>(this->_argChar) << std::endl;
-			std::cout << "float: " << static_cast<float>(this->_argChar) << ".0f" << std::endl;
-			std::cout << "double: " << static_cast<double>(this->_argChar) << ".0" << std::endl;
+			std::cout << "char: " << "'" << this->_argChar << "'" << std::endl;
+			std::cout << "int: " << this->_argInt << std::endl;
+			std::cout << "float: " << this->_argFloat << ".0f" << std::endl;
+			std::cout << "double: " << this->_argDouble << ".0" << std::endl;
 			break ;
 		case INT :
-			std::cout << "char: " << static_cast<char>(this->_argInt) << std::endl;
+			if (this->_argChar == -1)
+				std::cout << "char: " <<  "Non displayable" << std::endl;
+			else
+				std::cout << "char: " <<  "'" << this->_argChar <<  "'" << std::endl;
 			std::cout << "int: " << this->_argInt << std::endl;
-			std::cout << "float: " << static_cast<float>(this->_argInt) << ".0f" << std::endl;
-			std::cout << "double: " << static_cast<double>(this->_argInt) << ".0" << std::endl;
+			std::cout << "float: " << this->_argFloat << ".0f" << std::endl;
+			std::cout << "double: " << this->_argDouble << ".0" << std::endl;
 			break ;
 		case FLOAT :
-			std::cout << "char: " << "impossible" << std::endl;
-			std::cout << "int: " << "impossible" << std::endl;
-			if (this->_arg == "-inff" || this->_arg == "+inff" || this->_arg == "nanf")
-				std::cout << "float: " << this->_arg << std::endl;
+			if (this->_argChar == -1)
+				std::cout << "char: " <<  "Non displayable" << std::endl;
+			else if (this->_argChar == -2)
+				std::cout << "char: " <<  "impossible" << std::endl;
 			else
-				std::cout << "float: " << this->_argDouble << std::endl;		
+				std::cout << "char: " <<  "'" << this->_argChar <<  "'" << std::endl;
 			if (this->_arg == "-inff" || this->_arg == "+inff" || this->_arg == "nanf")
-				std::cout << "double: " << this->_arg.substr(0, this->_arg.size() - 1) << std::endl;
+				std::cout << "int: " << "impossible" << std::endl;
 			else
-				std::cout << "double: " << this->_argDouble << std::endl;
+				std::cout << "int: " << this->_argInt << std::endl;
+			std::cout << "float: " << this->_argFloat;
+			if (this->_arg[this->_arg.size() - 3] == '.' && this->_arg[this->_arg.size() - 2] == '0')
+				std::cout << ".0";
+			std::cout << "f" << std::endl;		
+			std::cout << "double: " << this->_argDouble;
+			if (this->_arg[this->_arg.size() - 3] == '.' && this->_arg[this->_arg.size() - 2] == '0')
+				std::cout << ".0";
+			std::cout << std::endl;
 			break ;
 		case DOUBLE :
-			std::cout << "char: " << "impossible" << std::endl;
-			std::cout << "int: " << "impossible" << std::endl;
-			if (this->_arg == "-inf" || this->_arg == "+inf" || this->_arg == "nan")
-				std::cout << "float: " << this->_arg << "f" << std::endl;
+			if (this->_argChar == -1)
+				std::cout << "char: " <<  "Non displayable" << std::endl;
+			else if (this->_argChar == -2)
+				std::cout << "char: " <<  "impossible" << std::endl;
 			else
-				std::cout << "float: " << static_cast<float>(this->_argDouble) << "f" << std::endl;		
+				std::cout << "char: " <<  "'" << this->_argChar <<  "'" << std::endl;
 			if (this->_arg == "-inf" || this->_arg == "+inf" || this->_arg == "nan")
-				std::cout << "double: " << this->_arg << std::endl;
+				std::cout << "int: " << "impossible" << std::endl;
 			else
-				std::cout << "double: " << this->_argDouble << std::endl;
+				std::cout << "int: " << this->_argInt << std::endl;
+			std::cout << "float: " << this->_argFloat;
+			if (this->_arg[this->_arg.size() - 2] == '.' && this->_arg[this->_arg.size() - 1] == '0')
+				std::cout << ".0";
+			std::cout << "f" << std::endl;		
+			std::cout << "double: " << this->_argDouble;
+			if (this->_arg[this->_arg.size() - 2] == '.' && this->_arg[this->_arg.size() - 1] == '0')
+				std::cout << ".0";
+			std::cout << std::endl;
 			break ;
 		default :
 			std::cout << "bite" << std::endl;
@@ -93,6 +112,9 @@ bool	Convert::_isChar(std::string str) {
 	if (str.size() == 1 && !std::isdigit(str[0]))
 	{
 		this->_argChar = str[0];
+		this->_argInt = static_cast<int>(this->_argChar);
+		this->_argFloat = static_cast<float>(this->_argChar);
+		this->_argDouble = static_cast<double>(this->_argChar);
 		return (true);
 	}
 	return (false);
@@ -107,37 +129,63 @@ bool	Convert::_isInt(std::string str) {
 		if (!std::isdigit(str[i]))
 			return (false);
 	this->_argInt = std::strtol(str.c_str(), NULL, 10);
+	if (this->_argInt < 32 || this->_argInt > 126)
+		this->_argChar = -1;
+	else
+		this->_argChar = static_cast<char>(this->_argInt);
+	this->_argFloat = static_cast<float>(this->_argInt);
+	this->_argDouble = static_cast<double>(this->_argInt);
 	return (true);
 }
 
 bool	Convert::_isFloat(std::string str) {
-	if (str == "-inff" || str == "+inff" || str == "nanf")
-		return (true);
-	unsigned long i = 0;
-	if (str[0] == '-')
-		i++;
-	for (; i < str.size() - 1; i++) {
-		if (!std::isdigit(str[i]) && str[i] != '.')
+	if (str != "-inff" && str != "+inff" && str != "nanf") {
+		unsigned long i = 0;
+		if (str[0] == '-')
+			i++;
+		for (; i < str.size() - 1; i++) {
+			if (!std::isdigit(str[i]) && str[i] != '.')
+				return (false);
+		}
+		if (str[i] != 'f')
 			return (false);
 	}
-	if (str[i] != 'f')
-		return (false);
 	this->_argFloat = std::strtof(str.c_str(), NULL);
+	this->_argInt = static_cast<int>(this->_argFloat);
+	if (str[str.size() - 3] == '.' && str[str.size() - 2] == '0' && str[str.size() - 1] == 'f') {
+		if (this->_argInt >= 32 && this->_argInt <= 126)
+			this->_argChar = static_cast<char>(this->_argFloat);
+		else
+			this->_argChar = -1;
+	}
+	else
+		this->_argChar = -2;
+	this->_argDouble = static_cast<double>(this->_argFloat);
 	return (true);
 }
 
 bool	Convert::_isDouble(std::string str) {
-	if (str == "-inf" || str == "+inf" || str == "nan")
-		return (true);
-	unsigned long i = 0;
-	if (str[0] == '-')
-		i++;
-	for (; i < str.size() - 1; i++) {
-		if (!std::isdigit(str[i]) && str[i] != '.')
+	if (str != "-inf" && str != "+inf" && str != "nan") {
+		unsigned long i = 0;
+		if (str[0] == '-')
+			i++;
+		for (; i < str.size() - 1; i++) {
+			if (!std::isdigit(str[i]) && str[i] != '.')
+				return (false);
+		}
+		if (str[i] == 'f')
 			return (false);
 	}
-	if (str[i] == 'f')
-		return (false);
 	this->_argDouble = std::strtod(str.c_str(), NULL);
+	this->_argInt = static_cast<int>(this->_argDouble);
+	if (str[str.size() - 2] == '.' && str[str.size() - 1] == '0') {
+		if (this->_argInt >= 32 && this->_argInt <= 126)
+			this->_argChar = static_cast<char>(this->_argDouble);
+		else
+			this->_argChar = -1;
+	}
+	else
+		this->_argChar = -2;
+	this->_argFloat = static_cast<float>(this->_argDouble);
 	return (true);
 }
